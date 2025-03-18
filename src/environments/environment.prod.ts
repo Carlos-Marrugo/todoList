2c7d96e61e1b162d31a7d009926354dba697caac
Carlos Marrugo <140767174+Carlos-Marrugo@users.noreply.github.com>
2025-03-18 14:13:19 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,5 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDmxv8eW1GGSZR9-MEDr7JwmXQ9XZcq2l0",
-    authDomain: "todolist-6acf3.firebaseapp.com",
-    projectId: "todolist-6acf3",
-    storageBucket: "todolist-6acf3.firebasestorage.app",
-    messagingSenderId: "754951096786",
-    appId: "1:754951096786:web:66d067ec7b86ecf57ee00b",
-    measurementId: "G-MZ82Q9KV9X"
   }
 };
